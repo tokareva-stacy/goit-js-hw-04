edc942c5d9d02083000b20e8861666d4284cc0f2
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-05-30 12:32:56 +0300

    add files

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,11 @@
-function filterArray(numbers, value) {
-    const result = [];
-    for (let i = 0; i < numbers.length; i++) {
-        if (numbers[i] > value) {
-            result.push(numbers[i]); 
-        }
-      }
-    
-      return result;
-}
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
