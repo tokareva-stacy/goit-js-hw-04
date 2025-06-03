295c2ff3caccd78ce1651b62c8b6afbc9017924a
Nastia Tokarieva <tokarevanyura@gmail.com>
2025-06-03 23:13:04 +0300

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,12 @@
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
 
+  for (const quantity of Object.values(products)) {
+    totalQuantity += quantity;
+  }
+
+  return totalQuantity <= containerSize;
+}
 
 
 console.log(
